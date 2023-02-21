3e921995d9988bf5aa62eb7d00c91b1d19d400c2
JINJIN <jinjin_no.1@kakao.com>
2023-02-21 18:05:03 +0900

    프로젝트프로젝트

---
+++ ProjectExample/src/com/dice/exe/ExeApp.java
@@ -0,0 +1,23 @@
+package com.dice.exe;
+
+import com.dice.service.Dice;
+
+public class ExeApp {
+	//실행 클래스
+	//기능을 구현해놓거나 객체를 만들 용도
+	public static void main(String[] args) {
+		//시작하는 곳이자 종료되는 곳->생성함과 동시에 종료됨
+		
+		//생성자를 통해 프로그램 실행
+		new Dice(); //하나의 객체를 만든 방식 
+		
+//		//객체를 통해 프로그램 실행
+//		Dice dice = new Dice();
+//		dice.run();
+	
+	}
+	
+	
+	
+	
+}
